0e39bf7faf13731425059a573dd68d48f6997110
Dimitr-Remicio <113401276+Dimitr-Remicio@users.noreply.github.com>
2023-05-24 18:46:54 -0500

    practicing

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,55 @@
 export const App = () => {
   return (
-    <div
+    <>
+    <div 
       style={{
-        height: '100vh',
+        display: 'flex',
+        alignItems:'center',
+        justifyContent: 'center',
+        width:'auto',
+        height:'200px',
+        marginBottom:'20px',
+        borderRadius:'30px',
+        boxShadow: '0px 0px 10px 1px #232124',
+        backgroundImage:'url(https://res.cloudinary.com/practicaldev/image/fetch/s--fced_LNQ--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/i/1zg83mt0lo13dfmff1cr.png)',
+        backgroundSize:'cover',
+      }} />
+    
+    <div
+      className="title"
+      style={{
+        height: 'auto',
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#ebeced'
+      }}
+    > Goit React HW-01 comp...
+    </div>
+    </>
+    
+  );
+  
+};
+
+export const Text = () => {
+  return (
+    <div className="text"
+      style={{
+        height: 'auto',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 25,
+        color: '#6ADAF8'
       }}
     >
-      React homework template
+      se ha deployado correctamente.....
     </div>
+    
   );
+  
 };
+
+
